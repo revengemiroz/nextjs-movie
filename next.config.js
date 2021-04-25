158e101f14a574166d46f9086daf3d2c183723c3
revengemiroz <mirozxy@gmail.com>
2021-04-25 13:07:50 +0545

    add search bar

---
+++ next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  env: {
+    imageBaseUrl: "https://image.tmdb.org/t/p/w500",
+    fetchBaseUrl: "https://api.themoviedb.org/3",
+    NEXT_PUBLIC_API_KEY: process.env.NEXT_PUBLIC_API_KEY,
+  },
+};
