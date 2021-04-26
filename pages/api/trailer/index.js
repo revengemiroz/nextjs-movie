b36736b31733890a1c0b39b3d6debf1d30fd56fb
revengemiroz <miroz.devkota@gmail.com>
2021-04-26 15:25:12 +0545

    genre list fix

---
+++ pages/api/trailer/index.js
@@ -0,0 +1,15 @@
+import tmdb from "../../../utils/tmdb";
+
+export default async (req, res) => {
+  const { page } = req.query;
+  try {
+    const { data } = await tmdb.get(`/movie/popular`, {
+      params: {
+        page: page,
+      },
+    });
+    res.status(200).send({ data });
+  } catch (error) {
+    res.status(400).send({ error });
+  }
+};
